e847552ef787632385d0ca98160b754b6a345df3
Ivan <paster-ivan@yandex.by>
2022-07-14 23:09:59 +0300

    15 lesson

--- src/api/todolistsAPI.ts
+++ src/api/todolistsAPI.ts
@@ -1,5 +1,55 @@
 import axios, {AxiosResponse} from 'axios'
 
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    withCredentials: true,
+    headers: {
+        'API-KEY': '49e19b54-12f4-44c4-af64-14bf246effef',
+    }
+})
+
+export const todolistsAPI = {
+    getTodolists() {
+        return instance.get<Array<TodolistType>>(`/todo-lists/`)
+    },
+    createTodolist(title: string) {
+        return instance.post<{ title: string }, AxiosResponse<ResponseType<{ item: TodolistType }>>>('todo-lists', {title});
+    },
+    deleteTodolist(todolistId: string) {
+        return instance.delete<ResponseType>(`/todo-lists/${todolistId}`)
+    },
+    updateTodolistTitle(todolistId: string, title: string) {
+        return instance.put<{ title: string }, AxiosResponse<ResponseType>>(`todo-lists/${todolistId}`, {title});
+    },
+}
+
+export const tasksAPI = {
+    getTasks(todolistId: string) {
+        return instance.get<GetTaskResponseType>(`/todo-lists/${todolistId}/tasks`)
+    },
+    createTask(todolistId: string, title: string) {
+        return instance.post<{ title: string }, AxiosResponse<ResponseType<{ item: TaskType }>>>(`todo-lists/${todolistId}/tasks`, {title});
+    },
+    deleteTask(todolistId: string, taskId: string) {
+        return instance.delete<ResponseType>(`/todo-lists/${todolistId}/tasks/${taskId}`)
+    },
+    updateTask(todolistId: string, taskId: string, model: UpdateTaskModelType) {
+        return instance.put<UpdateTaskModelType, AxiosResponse<ResponseType<{ item: TaskType }>>>(`todo-lists/${todolistId}/tasks/${taskId}`, model);
+    }
+}
+
+export const authAPI = {
+    login(data: LoginParamsType) {
+        return instance.post<LoginParamsType, AxiosResponse<ResponseType<{ userId: number }>>>('auth/login', data);
+    },
+    me() {
+        return instance.get<ResponseType<AuthType>>('auth/me')
+    },
+    logout() {
+        return instance.delete<ResponseType>('auth/login')
+    }
+}
+
 export type TodolistType = {
     id: string
     addedDate: string
@@ -60,48 +110,15 @@ export type UpdateTaskModelType = {
     deadline: string
 }
 
-const instance = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
-    withCredentials: true,
-    headers: {
-        'API-KEY': '49e19b54-12f4-44c4-af64-14bf246effef',
-    }
-})
-
-export const todolistsAPI = {
-
-    getTodolists() {
-        return instance.get<Array<TodolistType>>(`/todo-lists/`)
-    },
-
-    createTodolist(title: string) {
-        return instance.post<{ title: string }, AxiosResponse<ResponseType<{ item: TodolistType }>>>('todo-lists', {title});
-    },
-
-    deleteTodolist(todolistId: string) {
-        return instance.delete<ResponseType>(`/todo-lists/${todolistId}`)
-    },
-
-    updateTodolistTitle(todolistId: string, title: string) {
-        return instance.put<{ title: string }, AxiosResponse<ResponseType>>(`todo-lists/${todolistId}`, {title});
-    },
+export type LoginParamsType = {
+    email: string
+    password: string,
+    rememberMe?: boolean
+    captcha?: string
 }
 
-export const tasksAPI = {
-
-    getTasks(todolistId: string) {
-        return instance.get<GetTaskResponseType>(`/todo-lists/${todolistId}/tasks`)
-    },
-
-    createTask(todolistId: string, title: string) {
-        return instance.post<{ title: string }, AxiosResponse<ResponseType<{ item: TaskType }>>>(`todo-lists/${todolistId}/tasks`, {title});
-    },
-
-    deleteTask(todolistId: string, taskId: string) {
-        return instance.delete<ResponseType>(`/todo-lists/${todolistId}/tasks/${taskId}`)
-    },
-
-    updateTask(todolistId: string, taskId: string, model: UpdateTaskModelType) {
-        return instance.put<UpdateTaskModelType, AxiosResponse<ResponseType<{ item: TaskType }>>>(`todo-lists/${todolistId}/tasks/${taskId}`, model);
-    }
+export type AuthType = {
+    id: number,
+    email: string,
+    login: string
 }
